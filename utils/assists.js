5e5cd6ca7051ee7156f0d633e2ee61ad79af9c7a
屹兮 <3616721518@qq.com>
2026-08-18 19:50:51 +0800

    fix(icon): 语雀图标改用纯 SVG 矢量绘制，避免 base64 PNG 在 GitHub Pages CSP 下裂图

--- utils/assists.js
+++ utils/assists.js
@@ -1,27 +1,8 @@
-export const YuQueSVG = '<svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="96px" height="96px" viewBox="0 0 96 96" enable-background="new 0 0 96 96" xml:space="preserve">  <image id="image0" width="96" height="96" x="0" y="0"\n' +
-  '    href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAMAAADVRocKAAAABGdBTUEAALGPC/xhBQAAACBjSFJN\n' +
-  'AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAA+VBMVEVMaXEpyHMox3Iox3Io\n' +
-  'x3Iox3Iox3Iox3Iox3IoyHIoyHIoyHIoyHMoyHIoyHIpyHMsy3gsynIpyHRN1nUoyHMoyHJp\n' +
-  '6YG8/8gsy3UoyHMqyXQpyXRm21he2mMoyHJs4WB45WOF7WRp3Flp3Ftr3Fxr3F1r3F9r3mFq3WBr\n' +
-  '3VuJ6GJE0GmS512Q5lyT51yU6F+U51yT5lyT5lyV512W6V+T512T51yT5lyU51yT5l2U5l2U51yT\n' +
-  '5lyT5lyT5lwox3I7zWxW1mFg2Vxj2lhh2ldf2Vc3zG0tyXBN02RE0Gdb115131l74FqB4lqF41uK\n' +
-  '5FuT5lz///+r7OK9AAAAQHRSTlMAXubt8/j+3dXOwrinmod7RBZROgxlcQYBH54yKP1ckUUsE/jw\n' +
-  '2sGlhGPqJ+to/rFAhO/nTh2h18V7kZ6W0rvcWkLQpwAAAAFiS0dEUg1gLZAAAAAHdElNRQfnAgoL\n' +
-  'DAEFD6LlAAADEUlEQVRo3u2Z6ULaUBCFWRKBhF0gqFgptmhrWSJQtChGSFLp3vd/mbJIheZmltzQ\n' +
-  'X5wH+A53Zs6dJEQie+31/xWNxRX1YCFViccSyZSmpzPZXDRfCMuhqFpiJTKH4Tjk/RzmHrlSGA5l\n' +
-  'y1+xYhgOGcDB0kM4RCEOOaRCaHcFMrAS8g6GAjpoVWmHNGhg5aQNorCBeiRrcAQbWMfSTUAM1HK0\n' +
-  'mD+pFYKPrEWRmqnt6gRL6RLjWiPwczLDWiTwJfDIbbRURopvxDB+TO7Kq6AHKEvxCwrGV+QOcIwe\n' +
-  'QC7Jhwe7rVApic+o1PpP43zrRIJfJvBlDGoqxSAamF9IUPjBp8jQSPzAOahSGixTo6xlPU6mtuM6\n' +
-  'tj2dfAYMUoH45aeJ7W7Inj35OuQD8IuzLfxCzsTPQAvAP3UFsr+EFYVXtiuWzyHOmPy6H9/PIcZb\n' +
-  'ya9PXX/N5GvUgPiuIxzYCoNvnLugpiKDNMPgjYtIdARG1t5ifPerwEAl85so37VFNaJeeBc4X1wj\n' +
-  '4kvCpUMxEE0qbU7f2RS+MGykzV+n8YVdppwADPCmREkgvH00zol84QnwKcICDPcgjvKr78l80RTh\n' +
-  'KwcP8IsEayeL8ZsMvitYznmETwrwWo6gBQbMvyQGYCXBlCIrkxhgoMdwzOrUgD1XyNsCHeSTA+xf\n' +
-  'IfAiogf4WY8ePrgvGQH2O4AC7QJOgFfybhvw4ZoT4KW8Fx0Y4iaXb3tGSIce6i5IG3JTnmsoCd3T\n' +
-  'vAAv5Lmok1CDmQF2BQ8s4KdlZoBFDTiD6n/F5jv/REwF55MdYE+DdXDPG+gjrkfbl2gS/u+AH+At\n' +
-  '/oFWRF5p2AF2Xuqjajn0QbTJ5n9IzJXU9EwlT3iObn1b6/tf/Vjq50q/1mqv1Gng0A112795Mpnv\n' +
-  'kNc8fK/PwkcGPR7/I688kcZwpz8/Urrh4K9vud+ajA4D3/7ErM5cI8avvxuw8RGTjB+2gnyIa1E7\n' +
-  'O+oGoM/Vve3gM3pvdqX+2xv0zbFf0npjsx+g8AKVrrotczS+Hz489OYa3nTuzFZI7L32ktIfTxcr\n' +
-  'DiV1H4cAAAAldEVYdGRhdGU6Y3JlYXRlADIwMjMtMDItMTBUMTE6MTI6MDErMDA6MDDlYzq6AAAA\n' +
-  'JXRFWHRkYXRlOm1vZGlmeQAyMDIzLTAyLTEwVDExOjEyOjAxKzAwOjAwlD6CBgAAACh0RVh0ZGF0\n' +
-  'ZTp0aW1lc3RhbXAAMjAyMy0wMi0xMFQxMToxMjowMSswMDowMMMro9kAAAAASUVORK5CYII=" />\n' +
+// 语雀图标 - 纯 SVG 矢量绘制（鸟形剪影 + 书页）
+// 不依赖 base64 PNG，避免在 GitHub Pages CSP 环境下渲染成裂图
+export const YuQueSVG = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">' +
+  '<path d="M3 7l9-4 9 4-9 4-9-4z"/>' +
+  '<path d="M3 7v6c0 1 4 3 9 3s9-2 9-3V7"/>' +
+  '<path d="M12 11v8"/>' +
+  '<circle cx="18" cy="6" r="3" fill="currentColor" stroke="none"/>' +
   '</svg>'
