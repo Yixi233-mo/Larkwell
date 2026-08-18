19f65384357bb4ca5062908301a08d1871f7cf4a
屹兮 <3616721518@qq.com>
2026-08-18 19:14:51 +0800

    fix(template): 同步 elog.format.js 插件规范修复

--- templates/private-repo/elog.format.js
+++ templates/private-repo/elog.format.js
@@ -1,5 +1,12 @@
-# Larkwell 私有内容仓库
-# 复制 elog.format.cjs 内容（与公共仓库格式化配置保持一致）
-module.exports = function (doc) {
-  return doc;
+/**
+ * Larkwell 私有内容仓库 - elog 文档处理拓展点
+ *
+ * elog 规范：require(formatExtPath) 后解构 { format } 方法
+ * format(doc) 接收文档对象，返回处理后的文档对象
+ */
+module.exports = {
+  format: function (doc) {
+    // 默认不做额外处理，保持 elog 原生输出
+    return doc;
+  }
 };
